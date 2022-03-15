d42964d3e3f90afffcba437b26a2776e35448c7b
Gino Tasis <gtasis84@gmail.com>
2022-03-15 09:31:37 -0400

    Working app, need to fix layout

--- MovieSearch/Controller/ViewController.swift
+++ MovieSearch/Controller/ViewController.swift
@@ -8,44 +8,101 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
+
     
     // MARK: - Properties
-    @IBOutlet weak var searchTextField: UITextField!
-    @IBOutlet weak var tableView: UITableView!
+ 
+    @IBOutlet weak var table: UITableView!
+    @IBOutlet weak var field: UITextField!
+
+    var movies = [Movie]()
     
-    var movies: [String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-     
-        tableView.delegate = self
-        tableView.dataSource = self
-        searchTextField.delegate = self
-        
-    }
-
-
-    @IBAction func searchButtonClicked(_ sender: UIButton) {
+        table.register(MovieTableViewCell.nib(), forCellReuseIdentifier: MovieTableViewCell.identifer)
+        table.delegate = self
+        table.dataSource = self
+        field.delegate = self
     }
     
+    func searchMovies() {
+        field.resignFirstResponder()
+        
+        guard let text = field.text, !text.isEmpty else {
+            return
+        }
+        
+        let query = text.replacingOccurrences(of: " ", with: "%20")
+
+        movies.removeAll()
+        
+        URLSession.shared.dataTask(with: URL(string: "https://api.themoviedb.org/3/search/movie?api_key=1a326e6f4f3b3cee865c27b54d44b22f&query=\(query)")!) { data, response, error in
+            
+            guard let data = data, error == nil else {
+                return
+            }
+
+            var result: MovieResult?
+            do {
+                result = try JSONDecoder().decode(MovieResult.self, from: data)
+                
+                
+            } catch {
+                print(error)
+            }
+            
+            guard let finalResult = result else {
+                return
+            }
+            
+            
+            let newMovies = finalResult.results
+            self.movies.append(contentsOf: newMovies)
+            
+            DispatchQueue.main.async {
+                self.table.reloadData()
+            }
+            
+        }.resume()
+    }
+    
+    
+    @IBAction func searchButton(_ sender: UIButton) {
+        searchMovies()
+        field.text = ""
+    }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        <#code#>
+        searchMovies()
+        field.text = ""
+        return true
     }
-    
-    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return movies.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: MovieTableViewCell.identifer, for: indexPath) as? MovieTableViewCell else { return UITableViewCell()}
+        cell.configure(with: movies[indexPath.row])
+        return cell
     }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        <#code#>
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
     }
-    
     
 }
 
+struct MovieResult: Codable {
+    let results: [Movie]
+}
+
+struct Movie: Codable {
+    
+    let original_title: String
+    let overview: String
+    let vote_average: Float
+    let poster_path: String?
+    
+}
---
+++ MovieSearch/View/MovieTableViewCell.swift
@@ -0,0 +1,48 @@
+//
+//  MovieTableViewCell.swift
+//  MovieSearch
+//
+//  Created by Gino Tasis on 3/14/22.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var movieDescription: UILabel!
+    @IBOutlet weak var movieRating: UILabel!
+    @IBOutlet weak var movieTitle: UILabel!
+    @IBOutlet weak var moviePoster: UIImageView!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    static let identifer = "MovieTableViewCell"
+    
+    static func nib() -> UINib {
+        
+        return UINib(nibName: "MovieTableViewCell", bundle: nil)
+        
+    }
+    
+    func configure(with model: Movie) {
+    
+        
+        self.movieTitle.text = model.original_title
+        self.movieRating?.text = String(model.vote_average)
+        self.movieDescription.text = model.overview
+        let url = "https://image.tmdb.org/t/p/w500/\(model.poster_path!)"
+        if let data = try? Data(contentsOf: URL(string: url)!) {
+            self.moviePoster.image = UIImage(data: data)
+        }
+        
+    }
+}
